27eb4e775db7903d09b585586c6928bbf86a9f69
RajaniVerma277 <rajaniverma507@gmail.com>
2025-04-19 10:55:21 +0530

    add batch file, and some modification in code

--- dashboard.js
+++ dashboard.js
@@ -2,21 +2,4 @@ function toggleDemo(id) {
     const list = document.getElementById(id);
     list.classList.toggle("hidden");
   }
-  function buyCourse(courseName, amount, redirectTo) {
-    const options = {
-      key: "rzp_test_YourKeyHere", // Use your Razorpay Test Key
-      amount: amount * 100,
-      currency: "INR",
-      name: "Coaching Center",
-      description: courseName + " Purchase",
-      handler: function (response) {
-        alert("Payment Successful! ID: " + response.razorpay_payment_id);
-        window.location.href = redirectTo;
-      },
-      theme: {
-        color: "#1976d2"
-      }
-    };
-    const rzp = new Razorpay(options);
-    rzp.open();
-  }
+  
